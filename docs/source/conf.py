7b3bdeab867e5c064a956476160874f00eebc20e
Jeremy Adams <jeremy.adams@ga4gh.org>
2019-08-23 12:32:33 -0400

    modifications for readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,15 +38,23 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+# Non-default settings
+master_doc = "index"
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_context = {
+    'css_files': [
+        '_static/theme_overrides.css', # overrides for wide tables in RTD theme
+    ]
+}
